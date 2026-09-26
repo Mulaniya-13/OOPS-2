46e2704fe7ae4ba23b2a04b35ce372188e6defb7
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-09-26 21:22:25 +0530

    complile time operator overloading

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -3,20 +3,58 @@ using namespace std;
 
 
 //Compile Time polymorphism -- 1.)Function overloading 
-class Print{
+// class Print{
+//     public:
+//     void show(int x){
+//         cout<<"int: "<<x<<endl;
+//     }
+
+//     void show(string str){
+//         cout<<"String: "<<str<<endl;
+//     }
+// };
+
+// int main(){
+//     Print obj1;
+//     obj1.show(20);
+//     obj1.show("Aashish Mulaniya");
+//     return 0;
+// }
+
+//Compile Time Polymorphism -- 2.) Operator overloading
+class Complex{
+    int real;
+    int img;
+    
     public:
-    void show(int x){
-        cout<<"int: "<<x<<endl;
+    Complex(int r,int i){
+        real=r;
+        img=i;
     }
 
-    void show(string str){
-        cout<<"String: "<<str<<endl;
+    void showNum(){
+        cout<<real<<"+"<<img<<"i"<<endl;
+    }
+
+    Complex operator + (Complex &c2){
+        int resReal=this->real + c2.real;
+        int resImg=this->img + c2.img;
+        Complex c3(resReal,resImg);
+        return c3;
     }
 };
 
+
 int main(){
-    Print obj1;
-    obj1.show(20);
-    obj1.show("Aashish Mulaniya");
+    Complex c1(2,4);
+    Complex c2(3,2);
+
+
+    c1.showNum();
+    c2.showNum();
+
+    Complex c3= c1+c2;
+    c3.showNum();
+
     return 0;
 }
